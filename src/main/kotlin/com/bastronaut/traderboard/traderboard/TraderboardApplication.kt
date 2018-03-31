31d262980f0834ef8e62c0a5b128a6851f7f8bfc
bastronaut <btsijtsma@gmail.com>
2018-03-31 22:04:24 +0200

    setup Spring boot initializer

---
+++ src/main/kotlin/com/bastronaut/traderboard/traderboard/TraderboardApplication.kt
@@ -0,0 +1,11 @@
+package com.bastronaut.traderboard.traderboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TraderboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<TraderboardApplication>(*args)
+}
---
+++ src/test/kotlin/com/bastronaut/traderboard/traderboard/TraderboardApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.bastronaut.traderboard.traderboard
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TraderboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
