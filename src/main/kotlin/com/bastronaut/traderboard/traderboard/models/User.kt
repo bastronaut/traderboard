389f370562725337ee22fe4745d97db458d74115
bastronaut <btsijtsma@gmail.com>
2018-04-01 10:07:43 +0200

    h2 persistence config, kotlin no-arg for hibernate exception

--- src/main/kotlin/com/bastronaut/traderboard/traderboard/models/User.kt
+++ src/main/kotlin/com/bastronaut/traderboard/traderboard/models/User.kt
@@ -10,14 +10,25 @@ import javax.persistence.Id
  * Annotations info:
  * @Entity param specifics that class is an entity
  * @Id specify the field as a primary key
- * @GeneratedValue defines the creation strategy
+ * @GeneratedValue defines the creation strategy - NOTE: seems that it doesnt matter what
+ * we pass to the constructor as id argument, it will be stored with an auto generated ID
  *
  * Seems that, when no default values are added for arguments, it generates a hybris
  * exception? org.hibernate.InstantiationException: No default constructor for entity: ..User
+ * Seems problematic when constructor parameters are more complex objects
+ *
+ * FIX: kotlin no-arg compiler plugin generates synthetic default constructor for
+ * class with @Entity (and some others?) annotation. Added kotlin-noarg to buildscript
+ * dependencies and apply plugins
+ *
  */
 @Entity
-class User(val name: String = "", val age: Int = 0,
+class User(val name: String, val age: Int,
            @Id @GeneratedValue(strategy = GenerationType.AUTO)
-           val id: Long = 0)
+           val id: Long = 0) {
+
+
+
+}
 
 
