ee8e614e9115b007cd927a3aceac9e354d84a684
bastronaut <btsijtsma@gmail.com>
2018-04-08 22:54:00 +0200

    failed experiments with mockito

--- src/test/kotlin/com/bastronaut/traderboard/traderboard/controllers/UserControllerTest.kt
+++ src/test/kotlin/com/bastronaut/traderboard/traderboard/controllers/UserControllerTest.kt
@@ -1,10 +1,15 @@
 package com.bastronaut.traderboard.traderboard.controllers
 
 import com.bastronaut.traderboard.traderboard.models.User
+import com.bastronaut.traderboard.traderboard.repositories.UserRepository
 import com.bastronaut.traderboard.traderboard.services.UserService
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -24,6 +29,9 @@ class UserControllerTest() {
 
     @MockBean
     lateinit var userService: UserService
+
+
+
 
     @Test
     fun testGetUsers() {
@@ -45,6 +53,18 @@ class UserControllerTest() {
         mockMvc.perform(MockMvcRequestBuilders.post("/users/testuser"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("username").value("testuser"))
+    }
+
+    @Test
+    fun testSomethingMockito() {
+        var mockUserRepository = Mockito.mock(UserRepository::class.java)
+        Mockito.`when`(mockUserRepository.findByUsername("testy")).thenReturn(User("testy", "testy@email.com", 1))
+        var userServiceMockito = UserService(mockUserRepository)
+        var userController = UserController(userServiceMockito)
+        var testUser = userController.getUserByUsername("test")
+        assertEquals(testUser?.username, "testy")
+        assertEquals(testUser?.email, "testy@email.com")
+        assertEquals(testUser?.id, 1)
     }
 
 
